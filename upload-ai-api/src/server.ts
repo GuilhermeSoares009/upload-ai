25eef81ce997792b92bbb779718ca5ace89971d8
Guilherme Soares <guilherme.sousa009@hotmail.com>
2023-09-15 21:25:19 -0300

    Subindo 2° parte do back completa, inciando a integração com a api do openai

--- upload-ai-api/src/server.ts
+++ upload-ai-api/src/server.ts
@@ -2,11 +2,13 @@ import { fastify } from 'fastify'
 import { prisma } from './lib/prisma'
 import { getAllPromptsRoute } from './routes/get-all-prompts';
 import { uploadVideoRoute } from './routes/upload.video';
+import { createTranscription } from './routes/create.trasncription';
 
 const app = fastify()
 
 app.register(getAllPromptsRoute);
 app.register(uploadVideoRoute);
+app.register(createTranscription);
 
 app.listen({
     port: 3333,
